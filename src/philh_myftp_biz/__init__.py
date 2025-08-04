f7cc6bce00de188a7be88536dfa4dff7699c9325
MineFartS <143120468+MineFartS@users.noreply.github.com>
2025-08-04 10:03:34 -0400

    Update __init__.py

--- src/philh_myftp_biz/__init__.py
+++ src/philh_myftp_biz/__init__.py
@@ -15,15 +15,18 @@ from . import (
 )
 
 # Declare Modules
-PC = modules.Module('PC')
-AD = modules.Module('AD')
-AI = modules.Module('AI')
-Ffmpeg = modules.Module('Ffmpeg')
-Minecraft = modules.Module('Minecraft')
-Package = modules.Module('Package')
-Plex = modules.Module('Plex')
-VMs = modules.Module('VMs')
-Website = modules.Module('Website')
-YouTube = modules.Module('YouTube')
-#NoIP = modules.Module('NoIP')
-#Bios = modules.Module('Bios')
+try:
+    PC = modules.Module('PC')
+    AD = modules.Module('AD')
+    AI = modules.Module('AI')
+    Ffmpeg = modules.Module('Ffmpeg')
+    Minecraft = modules.Module('Minecraft')
+    Package = modules.Module('Package')
+    Plex = modules.Module('Plex')
+    VMs = modules.Module('VMs')
+    Website = modules.Module('Website')
+    YouTube = modules.Module('YouTube')
+    #NoIP = modules.Module('NoIP')
+    #Bios = modules.Module('Bios')
+except:
+    pass
