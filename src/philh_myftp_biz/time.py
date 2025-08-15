e0f324d766b40ccba54475f9ce41ff3317ab0688
Phil Hunt <143120468+MineFartS@users.noreply.github.com>
2025-08-15 07:15:00 -0400

    Update time.py

--- src/philh_myftp_biz/time.py
+++ src/philh_myftp_biz/time.py
@@ -1,10 +1,33 @@
-import sys, time, math, datetime as dt
-from typing import Literal
+import sys, time, math
+import datetime as dt
 
-def every(s:int, n:int=sys.maxsize):
-    for _ in range(n):
+def sleep(s:int, print:bool=False):
+    if print:
+        print('Waiting ...')
+        for x in range(1, s+1):
+            print('{}/{} seconds'.format(x, s))
+            time.sleep(1)
+    else:
         time.sleep(s)
-        yield
+    
+    return True
+
+class every:
+    
+    def __init__(self, s:int, max_iters:int=sys.maxsize):
+        self.s = s
+        self.max_iters = max_iters
+
+    def __iter__(self):
+        self.x = 0
+        return self
+    
+    def __next__(self):
+        if self.x == self.max_iters:
+            raise StopIteration
+        else:
+            time.sleep(self.s)
+            return
 
 def toHMS(stamp):
     m, s = divmod(stamp, 60)
@@ -66,11 +89,11 @@ class from_stamp:
         self.second = self.dt.second
 
         self.unix = stamp
-
-        self.__unix__ = stamp
+        self.__unix = stamp
 
     def update(self):
-        if self.__unix__ == self.unix:
+
+        if self.__unix == self.unix:
 
             t = dt.datetime(
                 self.year,
@@ -82,10 +105,11 @@ class from_stamp:
             )
 
             self.unix = t.timestamp()
-            self.__unix__ = t.timestamp()
+            self.__unix = t.timestamp()
 
         else:
-            t = dt.datetime.fromtimestamp(self.unix)
+
+            t = from_stamp(self.unix)
 
             self.year = t.year
             self.month = t.month
@@ -94,24 +118,26 @@ class from_stamp:
             self.minute = t.minute
             self.second = t.second
     
-    def toString(self):
-        class self_:
-            None
+    class __toString:
+        def __init__(self, p:'from_stamp'):
+            self.year = str(p.year)
+            self.month = str(p.month)
+            self.day = str(p.day)
+            self.hour = str(p.hour)
+            self.minute = str(p.minute)
+            self.second = str(p.second)
+            self.unix = str(p.unix)
 
-        for name in dir(self):
-            value = getattr(self, name)
-            if isinstance(value, (int, float)):
-                setattr(self_, name, str(value))
-
-        return self_
+    def toString(self) -> __toString:
+        return self.__toString(self)
 
     def stamp(self, format):
         return self.dt.strftime(format)
 
-def now() -> Literal[from_stamp]:
+def now() -> from_stamp:
     return from_stamp(time.time())
 
-def from_string(string, separator='/', order='YMD') -> Literal[from_stamp]:
+def from_string(string, separator='/', order='YMD') -> from_stamp:
 
     split = string.split(separator)
 
@@ -123,5 +149,23 @@ def from_string(string, separator='/', order='YMD') -> Literal[from_stamp]:
     dt_ = dt.datetime.strptime(f'{Y}-{M}-{D}', "%Y-%m-%d")
     return from_stamp(dt_.timestamp())
 
-def get(*input) -> Literal[from_stamp]:
+def from_ymdhms(
+    year: int,
+    month: int,
+    day: int,
+    hour: int,
+    minute: int,
+    second: int,
+):
+    t = dt.datetime(
+        year,
+        month,
+        day,
+        hour,
+        minute,
+        second
+    )
+    return from_stamp(t.timestamp())
+
+def get(*input) -> from_stamp:
     return from_stamp(dt.datetime(*input).timestamp())
