db24ed0f5afc0ee7013b2c60f123ab62207a24c6
Phil Hunt <143120468+MineFartS@users.noreply.github.com>
2025-08-17 11:37:56 -0400

    Update __init__.py

--- src/philh_myftp_biz/__init__.py
+++ src/philh_myftp_biz/__init__.py
@@ -19,15 +19,18 @@ from . import (
 
 # =====================================
 
-PC = modules.Module('PC')
-AD = modules.Module('AD')
-AI = modules.Module('AI')
-Ffmpeg = modules.Module('Ffmpeg')
-Minecraft = modules.Module('Minecraft')
-Package = modules.Module('Package')
-Plex = modules.Module('Plex')
-VMs = modules.Module('VMs')
-Website = modules.Module('Website')
-YouTube = modules.Module('YouTube')
+try:
+    PC = modules.Module('PC')
+    AD = modules.Module('AD')
+    AI = modules.Module('AI')
+    Ffmpeg = modules.Module('Ffmpeg')
+    Minecraft = modules.Module('Minecraft')
+    Package = modules.Module('Package')
+    Plex = modules.Module('Plex')
+    VMs = modules.Module('VMs')
+    Website = modules.Module('Website')
+    YouTube = modules.Module('YouTube')
+except:
+    pass
 
 # =====================================
